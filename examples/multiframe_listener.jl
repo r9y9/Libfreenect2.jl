b1e7ca03828a98586229d4140e3dd84ce0b31e55
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-12-04 02:41:46 +0900

    draw captured color, ir and depth

--- examples/multiframe_listener.jl
+++ examples/multiframe_listener.jl
@@ -14,13 +14,20 @@ start(device)
 while true
     !hasNewFrame(listener) && continue
 
-    @time frames = waitForNewFrame(listener)
+    frames = waitForNewFrame(listener)
     color = frames[FrameType.COLOR]
     ir = frames[FrameType.IR]
     depth = frames[FrameType.DEPTH]
 
-    irarray = asarray(ir)
-    color_array = asarray(color);
+    colormat = cv2.Mat(height(color), width(color), cv2.CV_8UC4, data(color))
+    irmat = cv2.Mat_{Float32}(height(ir), width(ir), data(ir))
+    depthmat = cv2.Mat_{Float32}(height(depth), width(depth), data(depth))
+
+    # resize color image since it's a bit large to draw
+    colormat = cv2.resize(colormat, (width(color)/3, height(color)/3))
+    cv2.imshow("color", colormat)
+    cv2.imshow("ir", irmat ./ 65535.0)
+    cv2.imshow("depth", depthmat ./ 4500.0)
 
     map(release, [color, ir, depth])
 
